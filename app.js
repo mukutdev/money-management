201e6908c83b342f2c4b04cb75ce1bda87b320ea
Mukut Chakrabarty <mukutckb1@gmail.com>
2022-08-21 19:33:19 +0600

    calculate-function-fixed

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// declareing balance
+// declaring balance
 let balance;
 
 //  getting element value
@@ -10,7 +10,6 @@ function getElementByIdValue(elementId){
     }
       return getValue; 
 }
-
 // set innerText
 function setInnerText(id, text){
     document.getElementById(id).innerText = text;
@@ -19,14 +18,12 @@ function setInnerText(id, text){
 // Calculate total balance
 function calculateTotalBalance() {
     const getIncomeData = getElementByIdValue('income') 
-   const getFoodData = getElementByIdValue('food') 
-   const getRentData = getElementByIdValue('rent') 
-   const getClothesData = getElementByIdValue('clothes')
-
+    const getFoodData = getElementByIdValue('food') 
+    const getRentData = getElementByIdValue('rent') 
+    const getClothesData = getElementByIdValue('clothes')
     const expenses = getFoodData + getRentData + getClothesData
-    console.log(getFoodData, getIncomeData, getRentData, getClothesData);
 
-           balance = getIncomeData - expenses
+    balance = getIncomeData - expenses  //getting balance from global scope
       if(getIncomeData >= expenses){ 
         setInnerText('expenses', expenses)
         setInnerText('balance', balance)
@@ -45,8 +42,6 @@ function calculateSavings() {
     console.log(getSavingPercentage);
     const getSavingAmount = balance * getSavingPercentage / 100
     const remainingBalance = balance - getSavingAmount;
-
-    // document.getElementById('saving-amount').innerText = getSavingAmount
     setInnerText('saving-amount', getSavingAmount)
     setInnerText('remaining-balance', remainingBalance)
    }
